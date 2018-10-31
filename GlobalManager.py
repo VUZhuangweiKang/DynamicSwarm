3c272070113a1f0da0c63fba0acf2806bcfd50ba
VUZhuangweiKang <zhuangwei.kang@vanderbilt.edu>
2018-10-30 20:59:50 -0500

    update

---
+++ GlobalManager.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env /usr/local/bin/python
+# encoding: utf-8
+# Author: Zhuangwei Kang
+
+
+import utl
+from Messenger import *
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ Worker.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env /usr/local/bin/python
+# encoding: utf-8
+# Author: Zhuangwei Kang
+
+from Messenger import Messenger
+
+# define a REP socket
+messenger = Messenger.cs_bind()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
